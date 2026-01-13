e9c48cbc6f1a4d0bf77aa50733746b320bae2a84
BmnQuentin <quentinbmn@gmail.com>
2026-01-13 10:45:31 +0100

    fix seeder for eig

--- prisma/seeders/evenement-indesirable-grave.seed.ts
+++ prisma/seeders/evenement-indesirable-grave.seed.ts
@@ -2,14 +2,28 @@ import { fakerFR as faker } from "@faker-js/faker";
 
 import { EvenementIndesirableGrave } from "@/generated/prisma/client";
 
+const usedNumeroDossiers = new Set<string>();
+
 export const createFakeEvenementIndesirableGrave = (): Omit<
   EvenementIndesirableGrave,
   "id" | "structureDnaCode"
 > => {
+  let numeroDossier = faker.number
+    .int({ min: 1000000, max: 10000000 })
+    .toString();
+
+  while (usedNumeroDossiers.has(numeroDossier)) {
+    numeroDossier = faker.number
+      .int({ min: 1000000, max: 10000000 })
+      .toString();
+  }
+
+  usedNumeroDossiers.add(numeroDossier);
+
   return {
     declarationDate: faker.date.past(),
     evenementDate: faker.date.past(),
-    numeroDossier: faker.number.int({ min: 1000000, max: 10000000 }).toString(),
+    numeroDossier,
     type: faker.helpers.arrayElement([
       "Vol",
       "Comportement violent",
