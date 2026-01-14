e6f06f4b0102299b7c286bf7b92d6d9e766f839c
Alezco <benjaminmorali4@hotmail.fr>
2026-01-14 10:45:12 +0100

    :recycle: Code review fixes

--- src/app/components/common/DownloadItem.tsx
+++ src/app/components/common/DownloadItem.tsx
@@ -34,12 +34,7 @@ export const DownloadItem = ({ fileUpload }: Props): ReactElement => {
       );
       const startYear = getYearFromDate(fileUpload.startDate);
       const endYear = getYearFromDate(fileUpload.endDate);
-      if (
-        isNaN(startYear) ||
-        isNaN(endYear) ||
-        startYear === -1 ||
-        endYear === -1
-      ) {
+      if (startYear === -1 || endYear === -1) {
         return categoryLabel;
       }
       return `${categoryLabel} ${startYear} - ${endYear}`;
