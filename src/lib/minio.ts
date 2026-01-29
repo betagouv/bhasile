fb5549713241cc6cf336dfcd88f427fc9ace9666
BmnQuentin <quentinbmn@gmail.com>
2026-01-29 15:48:21 +0100

    remove logs

--- src/lib/minio.ts
+++ src/lib/minio.ts
@@ -1,26 +1,19 @@
 import { Client } from "minio";
 
 export const minioClient = new Client({
-    endPoint: process.env.S3_URL!,
-    accessKey: process.env.S3_ACCESS!,
-    secretKey: process.env.S3_SECRET!,
-    useSSL: true,
+  endPoint: process.env.S3_URL!,
+  accessKey: process.env.S3_ACCESS!,
+  secretKey: process.env.S3_SECRET!,
+  useSSL: true,
 });
 
 export const checkBucket = async (bucketName: string): Promise<void> => {
-    const exists = await minioClient.bucketExists(bucketName);
-    if (exists) {
-        console.log("Bucket " + bucketName + " exists.");
-    } else {
-        await minioClient.makeBucket(bucketName);
-        console.log("Bucket " + bucketName + " created.");
-    }
+  const exists = await minioClient.bucketExists(bucketName);
+  if (!exists) {
+    await minioClient.makeBucket(bucketName);
+  }
 };
 
-export const getObject = async (
-    bucketName: string,
-    objectName: string
-) => {
-    return minioClient.getObject(bucketName, objectName);
+export const getObject = async (bucketName: string, objectName: string) => {
+  return minioClient.getObject(bucketName, objectName);
 };
-
