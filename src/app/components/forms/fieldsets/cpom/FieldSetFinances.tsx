7bec2c2dfd5b9434b31bece184ccc101ba804f12
Florian <hi@florianpanchout.com>
2026-02-04 19:30:44 +0100

    🔧 Replace FieldSetFinances component with CpomTable in CpomModificationFinance page for improved functionality. Remove obsolete FieldSetFinances file and clean up console log from cpom.repository.ts to enhance code maintainability.

--- src/app/components/forms/fieldsets/cpom/FieldSetFinances.tsx
+++
@@ -1,9 +0,0 @@
-import { CpomTable } from "@/app/components/forms/finance/budget-tables/CpomTable";
-
-export const FieldSetFinances = () => {
-  return (
-    <div>
-      <CpomTable />
-    </div>
-  );
-};
