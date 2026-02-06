36c53842dd69895fba05cd2077b08a2beadf344f
Florian <hi@florianpanchout.com>
2026-02-06 08:55:30 +0100

    🔧 Enhance CPOM modification header to display date range. Updated formatCpomName function to include department information based on granularity, improving clarity in CPOM details. Added utility functions for better date handling.

--- src/app/(authenticated)/cpoms/[id]/_components/CpomModificationHeader.tsx
+++ src/app/(authenticated)/cpoms/[id]/_components/CpomModificationHeader.tsx
@@ -2,13 +2,15 @@
 
 import { ReactElement } from "react";
 
-import { formatCpomName } from "@/app/utils/cpom.util";
+import { computeCpomDates, formatCpomName } from "@/app/utils/cpom.util";
+import { getYearFromDate } from "@/app/utils/date.util";
 
 import { useCpomContext } from "../_context/CpomClientContext";
 
 export const CpomModificationHeader = (): ReactElement | null => {
   const { cpom } = useCpomContext();
 
+  const years = `${getYearFromDate(computeCpomDates(cpom).dateStart)} - ${getYearFromDate(computeCpomDates(cpom).dateEnd)}`;
   return cpom ? (
     <div className="sticky top-0 z-2 bg-lifted-grey">
       <div className="flex border-b border-b-border-default-grey px-6 py-3 items-center">
@@ -17,7 +19,10 @@ export const CpomModificationHeader = (): ReactElement | null => {
             <strong className="pr-3">Modifier un cpom</strong>
           </h2>
           <h3 className="text-title-blue-france fr-h6 mb-0">
-            <strong className="pr-2">{formatCpomName(cpom)}</strong>
+            <strong className="pr-2">{formatCpomName(cpom)}</strong>{" "}
+            <span className="text-title-grey font-normal text-lg italic">
+              {years}
+            </span>
           </h3>
         </div>
       </div>
