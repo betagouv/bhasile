8c83ae070aaf1f872112886f389c9979047bd170
Florian <hi@florianpanchout.com>
2026-02-12 15:12:21 +0100

    ✨ Fix adresse list by watching adresses field

--- src/app/components/forms/fieldsets/structure/FieldSetHebergement.tsx
+++ src/app/components/forms/fieldsets/structure/FieldSetHebergement.tsx
@@ -19,9 +19,12 @@ export const FieldSetHebergement = ({
   const { control, setValue, watch, getValues, setError } =
     parentFormContext || localForm;
 
+  const dnaCode = watch("dnaCode");
   const typeBati = watch("typeBati") || Repartition.DIFFUS;
   const adminAddress = watch("adresseAdministrativeComplete");
   const sameAddress = watch("sameAddress");
+
+  const adresses = (watch("adresses") || []) as FormAdresse[];
 
   const hebergementsContainerRef = useRef(null);
 
@@ -33,7 +36,7 @@ export const FieldSetHebergement = ({
 
   const handleAddAddress = () => {
     const newAddress: FormAdresse = {
-      structureDnaCode: getValues("dnaCode"),
+      structureDnaCode: dnaCode,
       adresseComplete: "",
       adresse: "",
       codePostal: "",
@@ -163,7 +166,7 @@ export const FieldSetHebergement = ({
             </div>
           )}
 
-        {((getValues("adresses") || []) as FormAdresse[]).map((_, index) => (
+        {adresses.map((_, index) => (
           <AdresseComponent
             key={index}
             index={index}
