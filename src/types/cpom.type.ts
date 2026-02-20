57d8da517247c393d118a3e148ce64a7450b66ba
Florian <hi@florianpanchout.com>
2026-02-20 13:09:48 +0100

    ✨ Add new CPOM types and structure definitions to enhance type safety and data modeling

--- src/types/cpom.type.ts
+++ src/types/cpom.type.ts
@@ -1,3 +1,5 @@
+import { ActeAdministratifApiType } from "@/schemas/api/acteAdministratif.schema";
+
 export const CpomGranularity = [
   "DEPARTEMENTALE",
   "INTERDEPARTEMENTALE",
@@ -5,3 +7,46 @@ export const CpomGranularity = [
 ] as const;
 
 export type CpomGranularity = (typeof CpomGranularity)[number];
+
+export type CpomMillesime = {
+  id: number;
+  year: number;
+  dotationDemandee?: number;
+  dotationAccordee?: number;
+  cumulResultatNet?: number;
+  repriseEtat?: number;
+  affectationReservesFondsDedies?: number;
+  reserveInvestissement?: number;
+  chargesNonReconductibles?: number;
+  reserveCompensationDeficits?: number;
+  reserveCompensationBFR?: number;
+  reserveCompensationAmortissements?: number;
+  fondsDedies?: number;
+  reportANouveau?: number;
+  autre?: number;
+  commentaire?: string;
+};
+
+export type CpomStructure = {
+  id: number;
+  cpomId: number;
+  cpom?: Cpom;
+  structureId: number;
+  dateStart?: string;
+  dateEnd?: string;
+};
+
+export type Cpom = {
+  id: number;
+  name?: string;
+  operateur?: Operateur;
+  operateurId?: number;
+  region?: string;
+  departements?: string[];
+  granularity?: CpomGranularity;
+  dateStart?: string;
+  dateEnd?: string;
+  cpomMillesimes?: CpomMillesime[];
+  actesAdministratifs?: ActeAdministratifApiType[];
+  structures?: CpomStructure[];
+};
