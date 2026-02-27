193f73a4fb11c967f5ae1be6a4772497d62437dd
Florian <hi@florianpanchout.com>
2026-02-27 09:59:44 +0100

    ✨ Enhance default values utilities to include structure creation year for typologies and millesimes

--- src/app/utils/structureTypology.util.ts
+++ src/app/utils/structureTypology.util.ts
@@ -1,20 +1,40 @@
 import { StructureTypologieApiType } from "@/schemas/api/structure-typologie.schema";
 import { structureTypologieSchemaTypeFormValues } from "@/schemas/forms/base/structureTypologie.schema";
 
+import { getYearRange } from "./date.util";
+
 export const getStructureTypologyDefaultValues = (
-  structureTypologies: StructureTypologieApiType[]
+  structureTypologies: StructureTypologieApiType[],
+  structureCreationYear: number
 ): structureTypologieSchemaTypeFormValues[] => {
-  return structureTypologies.map((structureTypology) => ({
-    ...structureTypology,
-    year: structureTypology?.year,
-    placesAutorisees: structureTypology?.placesAutorisees ?? undefined,
-    pmr: structureTypology?.pmr ?? undefined,
-    lgbt: structureTypology?.lgbt ?? undefined,
-    fvvTeh: structureTypology?.fvvTeh ?? undefined,
-    placesACreer: structureTypology?.placesACreer ?? undefined,
-    placesAFermer: structureTypology?.placesAFermer ?? undefined,
-    echeancePlacesACreer: structureTypology?.echeancePlacesACreer ?? undefined,
-    echeancePlacesAFermer:
-      structureTypology?.echeancePlacesAFermer ?? undefined,
-  })) as structureTypologieSchemaTypeFormValues[];
+  const { years } = getYearRange();
+  const yearsToDisplay = years.filter((year) => year >= structureCreationYear);
+
+  return Array(yearsToDisplay.length)
+    .fill({})
+    .map((_, index) => ({
+      year: yearsToDisplay[index],
+    }))
+    .map((emptyBudget) => {
+      const structureTypology = structureTypologies.find(
+        (budget) => budget.year === emptyBudget.year
+      );
+      if (structureTypology) {
+        return {
+          ...structureTypology,
+          year: structureTypology.year,
+          placesAutorisees: structureTypology.placesAutorisees ?? undefined,
+          pmr: structureTypology.pmr ?? undefined,
+          lgbt: structureTypology.lgbt ?? undefined,
+          fvvTeh: structureTypology.fvvTeh ?? undefined,
+          placesACreer: structureTypology.placesACreer ?? undefined,
+          placesAFermer: structureTypology.placesAFermer ?? undefined,
+          echeancePlacesACreer:
+            structureTypology.echeancePlacesACreer ?? undefined,
+          echeancePlacesAFermer:
+            structureTypology.echeancePlacesAFermer ?? undefined,
+        };
+      }
+      return emptyBudget;
+    }) as structureTypologieSchemaTypeFormValues[];
 };
