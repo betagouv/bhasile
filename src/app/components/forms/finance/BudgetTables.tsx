646a9cbdac812a678a58a030b69c593a2b9ff916
Florian <hi@florianpanchout.com>
2026-03-04 09:03:54 +0100

    🐛 Fix typo in BudgetTables component description for grammatical accuracy

--- src/app/components/forms/finance/BudgetTables.tsx
+++ src/app/components/forms/finance/BudgetTables.tsx
@@ -66,7 +66,7 @@ export const BudgetTables = () => {
             severity="info"
             title=""
             className="rounded [&_p]:flex [&_p]:items-center mb-8 w-fit [&_.fr-notice\_\_desc]:text-text-default-grey"
-            description={`L’historique des données budgétaires à l’échelle du CPOM a déjà été renseignées lors de la saisie du CPOM. Si vous constatez une erreur et voulez apporter une modification, contactez-nous : ${BHASILE_CONTACT_EMAIL}`}
+            description={`L’historique des données budgétaires à l’échelle du CPOM a déjà été renseigné lors de la saisie du CPOM. Si vous constatez une erreur et voulez apporter une modification, contactez-nous : ${BHASILE_CONTACT_EMAIL}`}
           />
           <StructureCpomTable canEdit={false} />
         </fieldset>
