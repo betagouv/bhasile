1a42607b659bd9b1b97a4515c51af6b6fddabc8f
BmnQuentin <quentinbmn@gmail.com>
2026-03-10 11:26:39 +0100

    removing links that were autmatically re set

--- prisma/migrations/20260310102620_clean_post_file_upload_migration/migration.sql
+++ prisma/migrations/20260310102620_clean_post_file_upload_migration/migration.sql
@@ -4,6 +4,7 @@
   - You are about to drop the column `structureMillesimeId` on the `Budget` table. All the data in the column will be lost.
   - You are about to drop the column `category` on the `FileUpload` table. All the data in the column will be lost.
   - You are about to drop the column `categoryName` on the `FileUpload` table. All the data in the column will be lost.
+  - You are about to drop the column `cpomId` on the `FileUpload` table. All the data in the column will be lost.
   - You are about to drop the column `date` on the `FileUpload` table. All the data in the column will be lost.
   - You are about to drop the column `endDate` on the `FileUpload` table. All the data in the column will be lost.
   - You are about to drop the column `granularity` on the `FileUpload` table. All the data in the column will be lost.
@@ -42,13 +43,13 @@ ALTER TABLE "Budget" DROP COLUMN "structureMillesimeId";
 -- AlterTable
 ALTER TABLE "FileUpload" DROP COLUMN "category",
 DROP COLUMN "categoryName",
+DROP COLUMN "cpomId",
 DROP COLUMN "date",
 DROP COLUMN "endDate",
 DROP COLUMN "granularity",
 DROP COLUMN "parentFileUploadId",
 DROP COLUMN "startDate",
-DROP COLUMN "structureDnaCode",
-ADD COLUMN     "structureId" INTEGER;
+DROP COLUMN "structureDnaCode";
 
 -- AlterTable
 ALTER TABLE "StructureMillesime" DROP COLUMN "budgetId",
@@ -56,9 +57,3 @@ DROP COLUMN "structureTypologieId";
 
 -- AlterTable
 ALTER TABLE "StructureTypologie" DROP COLUMN "structureMillesimeId";
-
--- AddForeignKey
-ALTER TABLE "FileUpload" ADD CONSTRAINT "FileUpload_structureId_fkey" FOREIGN KEY ("structureId") REFERENCES "Structure"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "FileUpload" ADD CONSTRAINT "FileUpload_cpomId_fkey" FOREIGN KEY ("cpomId") REFERENCES "Cpom"("id") ON DELETE SET NULL ON UPDATE CASCADE;
