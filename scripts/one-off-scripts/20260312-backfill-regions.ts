0f51e1c57c5cc99d11f8947c2e87e80ef45e6148
BmnQuentin <quentinbmn@gmail.com>
2026-03-12 18:30:00 +0100

    all good

--- scripts/one-off-scripts/20260312-backfill-regions.ts
+++ scripts/one-off-scripts/20260312-backfill-regions.ts
@@ -1,3 +1,8 @@
+// One-off script: remplissage régions et association aux départements
+// Usage: yarn one-off 20260312-backfill-regions
+
+import "dotenv/config";
+
 import { createPrismaClient } from "@/prisma-client";
 
 import { REGIONS } from "@/constants";
@@ -5,7 +10,9 @@ import { REGIONS } from "@/constants";
 const prisma = createPrismaClient();
 
 async function backfillRegions() {
-  console.log("➡️ Remplissage régions et association aux départements");
+  console.log(
+    "➡️ Démarrage du remplissage régions et association aux départements"
+  );
 
   await prisma.region.createMany({
     data: REGIONS.map((region) => ({ name: region.name, code: region.code })),
