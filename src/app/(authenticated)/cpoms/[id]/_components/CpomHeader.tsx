d90e7c496c835dd2b22b68f75ab470193022dc72
Florian <hi@florianpanchout.com>
2026-03-13 15:18:17 +0100

    🔄 Refactor CpomHeader component: extract title logic into a separate function for improved readability and maintainability, enhancing the clarity of the component's rendering logic.

--- src/app/(authenticated)/cpoms/[id]/_components/CpomHeader.tsx
+++ src/app/(authenticated)/cpoms/[id]/_components/CpomHeader.tsx
@@ -28,11 +28,7 @@ export const CpomHeader = (): ReactElement | null => {
         <div>
           <h2 className="text-title-blue-france text-xs uppercase mb-0">
             <strong className="pr-3">
-              {isModificationPath
-                ? "Modifier un CPOM"
-                : isAjoutPath
-                  ? "Ajouter un CPOM"
-                  : "CPOM"}
+              {getCpomTitle(isModificationPath, isAjoutPath)}
             </strong>
           </h2>
           <h3 className="text-title-blue-france fr-h6 mb-0">
@@ -46,4 +42,17 @@ export const CpomHeader = (): ReactElement | null => {
       {isRootPath && <CpomNavigationMenu />}
     </div>
   ) : null;
+};
+
+const getCpomTitle = (
+  isModificationPath: boolean,
+  isAjoutPath: boolean
+): string => {
+  if (isModificationPath) {
+    return "Modifier un CPOM";
+  }
+  if (isAjoutPath) {
+    return "Ajouter un CPOM";
+  }
+  return "CPOM";
 };
