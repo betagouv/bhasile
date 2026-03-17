b99cef2e287492b2f002c32be2b54fba478a76a7
Florian <hi@florianpanchout.com>
2026-03-17 08:58:01 +0100

    🔄 Refactor ModificationLayout components: simplify props handling by using PropsWithChildren for improved type safety and readability in CPOM and Structures modification pages.

--- src/app/(authenticated)/structures/[id]/modification/layout.tsx
+++ src/app/(authenticated)/structures/[id]/modification/layout.tsx
@@ -1,7 +1,5 @@
-export default function ModificationLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { PropsWithChildren } from "react";
+
+export default function ModificationLayout({ children }: PropsWithChildren) {
   return <div className="flex flex-col gap-2 px-2">{children}</div>;
 }
