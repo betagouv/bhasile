9bc1c0621d251e9b63d8a514c18ab58ee19226fc
BmnQuentin <quentinbmn@gmail.com>
2026-04-01 11:29:10 +0200

    change order for migrations

--- prisma/migrations/20260323091039_remove_old_regions/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `region` on the `Departement` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Departement" DROP COLUMN "region";
---
+++ prisma/migrations/20260401092836_remove_old_regions/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Departement" ALTER COLUMN "region" DROP NOT NULL;
