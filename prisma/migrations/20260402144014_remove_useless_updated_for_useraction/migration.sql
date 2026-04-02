be609bfa5d2509c12f664518f7fda985599ba6f5
BmnQuentin <59441455+BmnQuentin@users.noreply.github.com>
2026-04-02 16:47:10 +0200

    :timer_clock: Remove updatedAt for UserAction (#1128)

---
+++ prisma/migrations/20260402144014_remove_useless_updated_for_useraction/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updatedAt` on the `UserAction` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserAction" DROP COLUMN "updatedAt";
