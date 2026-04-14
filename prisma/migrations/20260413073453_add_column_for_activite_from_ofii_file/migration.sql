5e3b2e73a4e847c929e78b1b3788f214016b6d25
BmnQuentin <quentinbmn@gmail.com>
2026-04-14 17:35:29 +0200

    migration with more fields in dna

--- prisma/migrations/20260413073453_add_column_for_activite_from_ofii_file/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `placesVacantes` on the `Activite` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Activite" DROP COLUMN "placesVacantes",
-ADD COLUMN     "tauxOccupation" DECIMAL(65,30);
---
+++ prisma/migrations/20260414153312_improve_schema_following_multidna_migration/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `placesVacantes` on the `Activite` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Activite" DROP COLUMN "placesVacantes",
+ADD COLUMN     "tauxOccupation" DECIMAL(65,30);
+
+-- AlterTable
+ALTER TABLE "Dna" ADD COLUMN     "departementAdministratif" TEXT,
+ADD COLUMN     "directionTerritoriale" TEXT,
+ADD COLUMN     "nom" TEXT,
+ADD COLUMN     "nomOfii" TEXT,
+ADD COLUMN     "operateurId" INTEGER,
+ADD COLUMN     "type" "StructureType";
+
+-- AddForeignKey
+ALTER TABLE "Dna" ADD CONSTRAINT "Dna_departementAdministratif_fkey" FOREIGN KEY ("departementAdministratif") REFERENCES "Departement"("numero") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Dna" ADD CONSTRAINT "Dna_operateurId_fkey" FOREIGN KEY ("operateurId") REFERENCES "Operateur"("id") ON DELETE SET NULL ON UPDATE CASCADE;
