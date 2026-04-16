feaba74daaef596e7d7c0929295d0188e11b3c39
BmnQuentin <quentinbmn@gmail.com>
2026-04-16 14:13:29 +0200

    add other fields from ofii file for activite

--- prisma/migrations/20260416121239_improve_schema_following_multidna_migration/migration.sql
+++ prisma/migrations/20260416121239_improve_schema_following_multidna_migration/migration.sql
@@ -1,12 +1,18 @@
 /*
   Warnings:
 
+  - You are about to drop the column `placesIndisponibles` on the `Activite` table. All the data in the column will be lost.
   - You are about to drop the column `placesVacantes` on the `Activite` table. All the data in the column will be lost.
 
 */
 -- AlterTable
-ALTER TABLE "Activite" DROP COLUMN "placesVacantes",
-ADD COLUMN     "tauxOccupation" DECIMAL(65,30);
+ALTER TABLE "Activite" DROP COLUMN "placesIndisponibles",
+DROP COLUMN "placesVacantes",
+ADD COLUMN     "tauxBPI" DECIMAL(65,30),
+ADD COLUMN     "tauxDeboutes" DECIMAL(65,30),
+ADD COLUMN     "tauxIndisponibilite" DECIMAL(65,30),
+ADD COLUMN     "tauxOccupation" DECIMAL(65,30),
+ADD COLUMN     "totalPlacesIndisponibles" INTEGER;
 
 -- AlterTable
 ALTER TABLE "Dna" ADD COLUMN     "departementAdministratif" TEXT,
