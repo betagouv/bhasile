2e9324dff573717c3606078110a5750b307588f5
Alezco <benjamin.morali@outlook.fr>
2026-04-24 11:09:45 +0200

    :sparkles: Add operateur documents

--- src/app/api/operateurs/operateur.repository.ts
+++ src/app/api/operateurs/operateur.repository.ts
@@ -24,6 +24,7 @@ export const getPaginatedOperateurs = async ({
   page: number | null;
   search: string | null;
 }): Promise<OperateurStat[]> => {
+  // TODO : transformer en Prisma classique et ajouter la logique restante dans operateur.service.ts
   return prisma.$queryRaw(Prisma.sql`
     WITH dernier_millesime_structure_typologie AS (
       SELECT DISTINCT ON (st."structureId")
@@ -71,6 +72,7 @@ export const countOperateurs = async ({
 }: {
   search: string | null;
 }): Promise<number> => {
+  // TODO : ajouter le filtre sur parentId null et nbStructures 0
   return prisma.operateur.count({
     where: {
       parentId: null,
@@ -97,6 +99,11 @@ export const findOne = async (id: number): Promise<OperateurWithStructures> => {
         select: {
           lgbt: true,
           fvvTeh: true,
+        },
+      },
+      documents: {
+        include: {
+          fileUploads: true,
         },
       },
     },
