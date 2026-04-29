611bb9eb9e3c9a4509b57ec46e2a9730ae03f3e7
BmnQuentin <quentinbmn@gmail.com>
2026-04-29 18:07:11 +0200

    own component

--- src/app/(authenticated)/structures/page.tsx
+++ src/app/(authenticated)/structures/page.tsx
@@ -2,50 +2,17 @@
 
 import { sendEvent } from "@socialgouv/matomo-next";
 import dynamic from "next/dynamic";
-import {
-  ReactElement,
-  memo,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import { ReactElement, useCallback, useEffect, useMemo, useState } from "react";
 
 import { SegmentedControl } from "@/app/components/common/SegmentedControl";
 import { ListLoader } from "@/app/components/lists/ListLoader";
 import { usePersistStructuresSearchQuery } from "@/app/hooks/usePersistStructuresSearchQuery";
 import { useStructuresSearch } from "@/app/hooks/useStructuresSearch";
-import { cn } from "@/app/utils/classname.util";
 
-import { Filters } from "../../components/filters/Filters";
-import { SearchBar } from "./_components/SearchBar";
 import { StructuresTable } from "./_components/StructuresTable";
+import { Toolbar } from "./_components/Toolbar";
 
 type Visualization = "tableau" | "carte";
-
-const Toolbar = memo(
-  ({
-    variant,
-    totalStructures,
-  }: {
-    variant: Visualization;
-    totalStructures: number | undefined;
-  }): ReactElement => (
-    <div className="flex gap-2 justify-end items-center py-3.5 px-6">
-      <SearchBar placeholder="Code ou commune" inputId="structures-search" />
-      <Filters />
-      <p
-        className={cn(
-          "pl-3 text-mention-grey mb-0 min-w-24 text-right",
-          variant === "carte" && "bg-lifted-grey/90 rounded px-2 py-1"
-        )}
-      >
-        {totalStructures ?? 0} entrée
-        {(totalStructures ?? 0) > 1 ? "s" : ""}
-      </p>
-    </div>
-  )
-);
 
 export default function Structures(): ReactElement {
   const [selectedVisualization, setSelectedVisualization] =
