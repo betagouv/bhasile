35a8dc8433600afc04e84a9647e1cc7270061f06
BmnQuentin <quentinbmn@gmail.com>
2026-04-30 17:22:19 +0200

    with mig and comment

---
+++ prisma/migrations/20260430152102_first_migration_for_transformation/migration.sql
@@ -0,0 +1,135 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[structureId,structureTransformationId,year]` on the table `StructureMillesime` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[structureId,structureTransformationId,year]` on the table `StructureTypologie` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateEnum
+CREATE TYPE "TransformationType" AS ENUM ('CREATION', 'EXTENSION_ET_CREATION', 'FERMETURE');
+
+-- CreateEnum
+CREATE TYPE "TransformationSubtype" AS ENUM ('OUVERTURE_EX_NIHILO', 'OUVERTURE_DEPUIS_UNE_OU_PLUSIEURS_STRUCTURES', 'EXTENSION_FROM_SCRATCH', 'EXTENSION_DEPUIS_STRUCTURES_QUI_CONTRACTENT', 'EXTENSION_DEPUIS_STRUCTURES_QUI_FERMENT', 'CONTRACTION_AVEC_TRANSFERT_VERS_AUTRE_STRUCTURE', 'CONTRACTION_SANS_TRANSFERT_DE_PLACES', 'FERMETURE_AVEC_TRANSFERT_VERS_UNE_OU_PLUSIEURS_STRUCTURES', 'FERMETURE_SANS_TRANSFERT', 'TRANSFO_HUDA_VERS_CADA_EXISTANT_MEME_OPERATEUR', 'TRANSFO_HUDA_VERS_CADA_NOUVEAU_MEME_OPERATEUR', 'TRANSFO_HUDA_REMISE_EN_CONCURRENCE_DES_PLACES');
+
+-- CreateEnum
+CREATE TYPE "StructureTransformationType" AS ENUM ('CREATION', 'FERMETURE', 'CONTRACTION', 'EXTENSION');
+
+-- DropIndex
+DROP INDEX "StructureMillesime_structureId_year_key";
+
+-- DropIndex
+DROP INDEX "StructureTypologie_structureId_year_key";
+
+-- AlterTable
+ALTER TABLE "Adresse" ADD COLUMN     "structureTransformationId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Antenne" ADD COLUMN     "structureTransformationId" INTEGER,
+ALTER COLUMN "structureId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Contact" ADD COLUMN     "structureTransformationId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Finess" ADD COLUMN     "structureTransformationId" INTEGER,
+ALTER COLUMN "structureId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Form" ADD COLUMN     "transformationId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Structure" ADD COLUMN     "isArchived" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "StructureMillesime" ADD COLUMN     "structureTransformationId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "StructureTypologie" ADD COLUMN     "structureTransformationId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "Transformation" (
+    "id" SERIAL NOT NULL,
+    "type" "TransformationType" NOT NULL,
+    "subtype" "TransformationSubtype" NOT NULL,
+    "formId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Transformation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "StructureTransformation" (
+    "id" SERIAL NOT NULL,
+    "structureId" INTEGER NOT NULL,
+    "transformationId" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "type" "StructureTransformationType" NOT NULL,
+    "motif" TEXT,
+    "public" "PublicType",
+    "adresseAdministrative" TEXT,
+    "codePostalAdministratif" TEXT,
+    "communeAdministrative" TEXT,
+    "departementAdministratif" TEXT,
+    "latitude" DECIMAL(65,30),
+    "longitude" DECIMAL(65,30),
+    "nom" TEXT,
+    "placesAutorisees" INTEGER,
+    "pmr" INTEGER,
+    "lgbt" INTEGER,
+    "fvvTeh" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "StructureTransformation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DnaStructureTransformation" (
+    "id" SERIAL NOT NULL,
+    "dnaId" INTEGER NOT NULL,
+    "structureTransformationId" INTEGER NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DnaStructureTransformation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StructureMillesime_structureId_structureTransformationId_ye_key" ON "StructureMillesime"("structureId", "structureTransformationId", "year");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StructureTypologie_structureId_structureTransformationId_ye_key" ON "StructureTypologie"("structureId", "structureTransformationId", "year");
+
+-- AddForeignKey
+ALTER TABLE "Adresse" ADD CONSTRAINT "Adresse_structureTransformationId_fkey" FOREIGN KEY ("structureTransformationId") REFERENCES "StructureTransformation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_structureTransformationId_fkey" FOREIGN KEY ("structureTransformationId") REFERENCES "StructureTransformation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Antenne" ADD CONSTRAINT "Antenne_structureTransformationId_fkey" FOREIGN KEY ("structureTransformationId") REFERENCES "StructureTransformation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Finess" ADD CONSTRAINT "Finess_structureTransformationId_fkey" FOREIGN KEY ("structureTransformationId") REFERENCES "StructureTransformation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StructureMillesime" ADD CONSTRAINT "StructureMillesime_structureTransformationId_fkey" FOREIGN KEY ("structureTransformationId") REFERENCES "StructureTransformation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StructureTypologie" ADD CONSTRAINT "StructureTypologie_structureTransformationId_fkey" FOREIGN KEY ("structureTransformationId") REFERENCES "StructureTransformation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transformation" ADD CONSTRAINT "Transformation_formId_fkey" FOREIGN KEY ("formId") REFERENCES "Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StructureTransformation" ADD CONSTRAINT "StructureTransformation_structureId_fkey" FOREIGN KEY ("structureId") REFERENCES "Structure"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StructureTransformation" ADD CONSTRAINT "StructureTransformation_transformationId_fkey" FOREIGN KEY ("transformationId") REFERENCES "Transformation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DnaStructureTransformation" ADD CONSTRAINT "DnaStructureTransformation_dnaId_fkey" FOREIGN KEY ("dnaId") REFERENCES "Dna"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DnaStructureTransformation" ADD CONSTRAINT "DnaStructureTransformation_structureTransformationId_fkey" FOREIGN KEY ("structureTransformationId") REFERENCES "StructureTransformation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
