78542726e3a22f15841c625001b88e539ab1b146
Florian <hi@florianpanchout.com>
2026-05-04 10:17:44 +0200

    🔧 Update seeders to include structureTransformationId: modified type definitions in adresse, antenne, contact, finess, and structure-typologie seeders to accommodate new transformation relations.

--- prisma/seeders/finess.seed.ts
+++ prisma/seeders/finess.seed.ts
@@ -19,6 +19,7 @@ export const createFinessList = (
       list.push({
         code,
         structureId: structure.id,
+        structureTransformationId: null,
         description: faker.word.noun(),
         createdAt: faker.date.past(),
         updatedAt: faker.date.past(),
