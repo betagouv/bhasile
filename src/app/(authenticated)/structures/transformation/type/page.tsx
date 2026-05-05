15712d0f1f6a28019286cfe067ab44c77e1cb391
Florian <hi@florianpanchout.com>
2026-05-05 09:14:03 +0200

    ✨ Refactor TransformationSelectionPage to utilize async searchParams and simplify component structure

--- src/app/(authenticated)/structures/transformation/type/page.tsx
+++ src/app/(authenticated)/structures/transformation/type/page.tsx
@@ -1,22 +1,14 @@
-"use client";
+export default async function TransformationSelectionPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ type?: "huda" | "creation" | "base" }>;
+}) {
+  const { type } = await searchParams;
 
-import { useState } from "react";
-
-import { StructureSearch } from "@/app/components/structure-selection/StructureSearch";
-import { StructureType } from "@/types/structure.type";
-
-export default function TransformationSelectionPage() {
-  const [selectedStructuresId, setSelectedStructuresId] = useState<number[]>(
-    []
-  );
   return (
     <div>
-      <StructureSearch
-        selectedStructuresId={selectedStructuresId}
-        setSelectedStructuresId={setSelectedStructuresId}
-        fixedType={StructureType.HUDA}
-        multiple
-      />
+      <h1>Transformation - Selection</h1>
+      <p>type: {type ?? "non fourni"}</p>
     </div>
   );
 }
