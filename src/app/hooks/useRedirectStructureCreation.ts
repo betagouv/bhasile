5eb0038500b71b156e997c22ab9b45d782d3071d
Alezco <benjamin.morali@outlook.fr>
2026-05-13 11:52:00 +0200

    :bug: Quickfix for operateur bad redirect

--- src/app/hooks/useRedirectStructureCreation.ts
+++ src/app/hooks/useRedirectStructureCreation.ts
@@ -4,10 +4,10 @@ import { useParams, usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 import { StructureApiRead } from "@/schemas/api/structure.schema";
-import { AjoutIdentificationFormValues } from "@/schemas/forms/ajout/ajoutIdentification.schema";
-import { DocumentsFinanciersFlexibleFormValues } from "@/schemas/forms/base/documentFinancier.schema";
+// import { AjoutIdentificationFormValues } from "@/schemas/forms/ajout/ajoutIdentification.schema";
+// import { DocumentsFinanciersFlexibleFormValues } from "@/schemas/forms/base/documentFinancier.schema";
 
-import { useLocalStorage } from "./useLocalStorage";
+// import { useLocalStorage } from "./useLocalStorage";
 
 export function useRedirectStructureCreation() {
   const params = useParams();
@@ -15,28 +15,28 @@ export function useRedirectStructureCreation() {
   const router = useRouter();
   const id = params.id as string;
 
-  const { currentValue: localStorageIdentificationValues } = useLocalStorage<
-    Partial<AjoutIdentificationFormValues>
-  >(`ajout-structure-${id}-identification`, {});
+  // const { currentValue: localStorageIdentificationValues } = useLocalStorage<
+  //   Partial<AjoutIdentificationFormValues>
+  // >(`ajout-structure-${id}-identification`, {});
 
-  const { currentValue: localStorageDocumentsValues } = useLocalStorage<
-    Partial<DocumentsFinanciersFlexibleFormValues>
-  >(`ajout-structure-${id}-documents`, {});
+  // const { currentValue: localStorageDocumentsValues } = useLocalStorage<
+  //   Partial<DocumentsFinanciersFlexibleFormValues>
+  // >(`ajout-structure-${id}-documents`, {});
 
   // Redirect if we did not go through the selection page
-  useEffect(() => {
-    if (
-      !localStorageDocumentsValues?.structureMillesimes ||
-      !localStorageIdentificationValues?.type
-    ) {
-      router.replace(`/ajout-structure`);
-    }
-  }, [
-    localStorageDocumentsValues,
-    localStorageIdentificationValues,
-    router,
-    id,
-  ]);
+  // useEffect(() => {
+  //   if (
+  //     !localStorageDocumentsValues?.structureMillesimes ||
+  //     !localStorageIdentificationValues?.type
+  //   ) {
+  //     router.replace(`/ajout-structure`);
+  //   }
+  // }, [
+  //   localStorageDocumentsValues,
+  //   localStorageIdentificationValues,
+  //   router,
+  //   id,
+  // ]);
 
   // Redirect if the structure already exists
   useEffect(() => {
