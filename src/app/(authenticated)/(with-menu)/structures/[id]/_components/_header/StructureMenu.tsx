5bf093a7ea559ebf535a3dfe205d6dfbb8acf6aa
Florian <hi@florianpanchout.com>
2026-05-19 11:34:35 +0200

    ✨ Add transformations access buttons

---
+++ src/app/(authenticated)/(with-menu)/structures/[id]/_components/_header/StructureMenu.tsx
@@ -0,0 +1,34 @@
+import Button from "@codegouvfr/react-dsfr/Button";
+import Link from "next/link";
+
+import { useButtonsPanel } from "@/app/hooks/useButtonsPanel";
+
+export const StructureMenu = ({ structureId }: Props) => {
+  const { isPanelOpen, setIsPanelOpen, panelRef } = useButtonsPanel();
+  return (
+    <div className="relative" ref={panelRef}>
+      <Button
+        priority="tertiary no outline"
+        iconId="ri-more-2-fill"
+        title="Transformer HUDA en CADA"
+        onClick={() => {
+          setIsPanelOpen(!isPanelOpen);
+        }}
+      />
+      {isPanelOpen && (
+        <div className="absolute top-full right-0 flex flex-col items-end bg-white shadow-md z-50">
+          <Link
+            href={`/structures/transformation/type?structureId=${structureId}`}
+            className="whitespace-nowrap fr-btn fr-btn--tertiary-no-outline"
+          >
+            Extension, contraction ou fermeture
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+type Props = {
+  structureId: number;
+};
