3ba5bc3a90d7568f14944dade46113d0780f2413
Florian <florian.panchout@beta.gouv.fr>
2026-05-22 11:02:17 +0200

    ✨ Add autres actes administratifs to CPOM (#1266)

--- src/app/components/forms/cpom/FieldSetActesAdministratifs.tsx
+++ src/app/components/forms/cpom/FieldSetActesAdministratifs.tsx
@@ -1,23 +1,10 @@
-import { FormKind } from "@/types/global";
-
-import { MaxSizeNotice } from "../MaxSizeNotice";
 import { DatesAndDocuments } from "./DatesAndDocuments";
 
-export const FieldSetActesAdministratifs = ({ formKind }: Props) => {
+export const FieldSetActesAdministratifs = () => {
   return (
     <fieldset className="flex flex-col gap-6">
-      {formKind !== "modification" && (
-        <legend className="text-xl font-bold mb-4 text-title-blue-france">
-          Document de convention du CPOM
-        </legend>
-      )}
-      <MaxSizeNotice />
       <DatesAndDocuments />
       <hr />
     </fieldset>
   );
-};
-
-type Props = {
-  formKind?: FormKind;
 };
