db11f46b61209918fa7423427b8307604b5ede5b
Florian <hi@florianpanchout.com>
2026-05-28 08:46:34 +0200

    🌱 Fix seeders

--- prisma/seeders/operateur.seed.ts
+++ prisma/seeders/operateur.seed.ts
@@ -11,7 +11,12 @@ import {
 type OperateurWithActes = Operateur & {
   actesAdministratifs: Omit<
     ActeAdministratifWithFileUploads,
-    "id" | "operateurId" | "structureId" | "structureDnaCode" | "cpomId"
+    | "id"
+    | "operateurId"
+    | "structureId"
+    | "structureDnaCode"
+    | "cpomId"
+    | "structureTransformationId"
   >[];
 };
 
