aa46a78dfaf37ca4b4680aed6fce22c470b39978
BmnQuentin <59441455+BmnQuentin@users.noreply.github.com>
2026-06-18 12:19:08 +0200

    :bar_chart: Review of impact indicators (#1378)

--- scripts/views/001_structures_filling.sql
+++ scripts/views/001_structures_filling.sql
@@ -1,6 +1,6 @@
 -- Objective: determine finalisation status per structure based on forms
 CREATE OR REPLACE VIEW:"SCHEMA"."structures_filling" AS
-WITH -- Forms de finalisation par structure
+WITH
   finalisation_forms AS (
     SELECT
       f."structureId" AS "structureId",
@@ -25,30 +25,30 @@ WITH -- Forms de finalisation par structure
       fd."name" = 'finalisation'
   )
 SELECT
-  sc."id",
-  sc."codeBhasile",
+  sc."id" AS "id",
+  sc."code_bhasile" AS "code_bhasile",
   CASE
     WHEN ff."formId" IS NOT NULL
-    AND ff."formStatus" = TRUE THEN 'Finalisé agent' -- Finalisé agent : form existe et status = true
+    AND ff."formStatus" = TRUE THEN 'Finalisé agent'
     WHEN ff."formId" IS NOT NULL
-    AND ff."hasValidatedStep" = TRUE THEN 'En cours agent' -- En cours agent : form existe et au moins un step est VALIDE
-    WHEN ff."formId" IS NOT NULL THEN 'Finalisé opérateur' -- Finalisé opérateur : form existe
-    ELSE 'Non commencé' -- Non commencé : pas de form
+    AND ff."hasValidatedStep" = TRUE THEN 'En cours agent'
+    WHEN ff."formId" IS NOT NULL THEN 'Finalisé opérateur'
+    ELSE 'Non commencé'
+  END AS "finalisation_status_detail",
+  CASE
+    WHEN ff."formId" IS NOT NULL
+    AND ff."formStatus" = TRUE THEN 'Finalisé'
+    WHEN ff."formId" IS NOT NULL
+    AND ff."hasValidatedStep" = TRUE THEN 'En cours'
+    WHEN ff."formId" IS NOT NULL THEN 'En cours'
+    ELSE 'Non commencé'
   END AS "finalisation_status",
-  CASE
-    WHEN ff."formId" IS NOT NULL
-    AND ff."formStatus" = TRUE THEN 'Finalisé' -- Finalisé : form existe et status = true
-    WHEN ff."formId" IS NOT NULL
-    AND ff."hasValidatedStep" = TRUE THEN 'En cours' -- En cours : form existe et au moins un step est VALIDE
-    WHEN ff."formId" IS NOT NULL THEN 'En cours' -- Non finalisé : form existe mais aucun step VALIDE
-    ELSE 'Non commencé' -- Non commencé : pas de form
-  END AS "finalisation_simplifiee",
   sc."operateur" AS "operateur",
-  sc."structureType" AS "type",
+  sc."structure_type" AS "structure_type",
   s."public" AS "public",
   sc."region" AS "region",
-  sc."createdAt" AS "created_at",
-  sc."updatedAt" AS "updated_at"
+  sc."created_at" AS "created_at",
+  sc."updated_at" AS "updated_at"
 FROM
 :"SCHEMA"."structures_core" sc
   JOIN public."Structure" s ON s."id" = sc."id"
