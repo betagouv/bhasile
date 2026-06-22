dc8224a1516c0c40e9de6373ebe0532a124563f1
Benjamin Morali <benjaminmorali4@hotmail.fr>
2026-06-22 16:17:54 +0200

    :sparkles: Add finances stats block (#1366)

--- src/schemas/api/statistiques.schema.ts
+++ src/schemas/api/statistiques.schema.ts
@@ -1,3 +1,5 @@
+import { BudgetApiType } from "./budget.schema";
+
 // TODO: refactor with real API schema
 type StructureTypeStat = {
   label: string;
@@ -27,6 +29,32 @@ type TypesPlaces = {
   byYear: ByYearTypePlaces[];
 };
 
+type Finance = {
+  aggregation: string;
+  byYear: FinanceByYear[];
+};
+
+type FinanceByYear = {
+  year: number;
+  total: FinanceItem;
+  autorisees: FinanceItem;
+  subventionnees: FinanceItem;
+};
+
+type FinanceItem = {
+  dotationDemandee: number;
+  dotationAccordee: number;
+  totalETP: number;
+  tauxEncadrement: number | null;
+  coutJournalier: number | null;
+  totalProduits: number;
+  totalCharges: number;
+  resultatNet: number;
+  excedentCumule: number;
+  deficitCumule: number;
+  soldeCumule: number;
+};
+
 export type StatistiquesApiType = {
   totalStructures: number;
   totalCpoms: number;
@@ -34,6 +62,23 @@ export type StatistiquesApiType = {
   tauxEquipement: number;
   structuresAvecCpom: number;
   placesAutorisees: number;
+  dotationAnnuelle: number;
+  dotationAutorisees: number;
+  dotationSubventionnees: number;
+  ETP: number;
+  ETPAutorisees: number;
+  ETPSubventionnees: number;
+  tauxEncadrement: number;
+  tauxEncadrementAutorisees: number;
+  tauxEncadrementSubventionnees: number;
+  coutJournalier: number;
+  coutJournalierAutorisees: number;
+  coutJournalierSubventionnees: number;
+  budgets: (BudgetApiType & {
+    excedentCumule: number;
+    deficitCumule: number;
+    soldeCumule: number;
+  })[];
   placesPmr: number;
   placesLgbt: number;
   placesFvvTeh: number;
@@ -42,4 +87,5 @@ export type StatistiquesApiType = {
   typesPlaces: TypesPlaces[];
   structureTypes: StructureTypeStat[];
   structureBatis: StructureBatiStat[];
+  finance: Finance;
 };
