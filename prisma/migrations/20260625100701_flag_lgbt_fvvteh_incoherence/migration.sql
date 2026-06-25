5ea3aeb1ec4bd6c9d1d97460f126d028fe06e548
BmnQuentin <quentinbmn@gmail.com>
2026-06-25 12:07:18 +0200

    migration

---
+++ prisma/migrations/20260625100701_flag_lgbt_fvvteh_incoherence/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "reporting"."monthly_structures_global_quality_count" ADD COLUMN     "has_issue_incoherence_fvvteh_places" INTEGER,
+ADD COLUMN     "has_issue_incoherence_lgbt_places" INTEGER;
