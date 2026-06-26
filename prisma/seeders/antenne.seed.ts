0a3eb6cee61d089534458b8d1027e7a820c16f45
Florian <florian.panchout@beta.gouv.fr>
2026-06-26 09:35:27 +0200

    🌱 Correctly seed structure versions (#1397)

--- prisma/seeders/antenne.seed.ts
+++ prisma/seeders/antenne.seed.ts
@@ -2,21 +2,21 @@ import { fakerFR as faker } from "@faker-js/faker";
 
 import { Antenne } from "@/generated/prisma/client";
 
-type StructureForAntenne = {
-  id: number;
+type VersionForAntenne = {
+  structureVersionId: number;
 };
 
 export const createAntenneList = (
-  structures: StructureForAntenne[]
+  versions: VersionForAntenne[]
 ): Omit<Antenne, "id">[] => {
   const antenneList: Omit<Antenne, "id">[] = [];
 
-  for (const structure of structures) {
+  for (const version of versions) {
     const nbAntennes = faker.number.int({ min: 1, max: 3 });
     for (let i = 0; i < nbAntennes; i++) {
       antenneList.push({
-        structureId: structure.id,
-        structureVersionId: null,
+        structureId: null,
+        structureVersionId: version.structureVersionId,
         name: faker.lorem.words(2),
         adresse: faker.location.streetAddress(),
         codePostal: faker.location.zipCode(),
