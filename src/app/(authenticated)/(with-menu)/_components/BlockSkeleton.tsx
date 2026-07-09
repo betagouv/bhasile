b8d74f085fc4638c351387aa25e317abc398a1e0
Florian <hi@florianpanchout.com>
2026-07-09 16:09:53 +0200

    ✨ Enhance BlockSkeleton component with loading indicator
    - Added Loader component to display a loading animation while content is being fetched.
    - Updated the layout of the loading message for better visual alignment.

--- src/app/(authenticated)/(with-menu)/_components/BlockSkeleton.tsx
+++ src/app/(authenticated)/(with-menu)/_components/BlockSkeleton.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from "react";
 
+import Loader from "@/app/components/ui/Loader";
+
 import { BlockTitle } from "./BlockTitle";
 
 type Props = {
@@ -10,8 +12,9 @@ type Props = {
 export const BlockSkeleton = ({ title, icon }: Props): ReactElement => (
   <section className="m-6 rounded-lg border border-default-grey bg-white">
     <BlockTitle title={title} iconClassName={icon} />
-    <p className="border-t border-default-grey px-4 py-6 text-sm text-mention-grey">
-      Chargement…
+    <p className="flex gap-2 items-center border-t border-default-grey px-4 py-6 text-sm text-mention-grey">
+      <Loader />
+      Chargement
     </p>
   </section>
 );
