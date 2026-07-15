41f022c4b68fa1b37022edba56076605a2d17ed2
Florian <florian.panchout@beta.gouv.fr>
2026-07-15 16:30:22 +0200

    ✨ D'autres retours de Clara sur les transfo (#1490)

--- src/app/components/forms/ContractualisationHuda.tsx
+++ src/app/components/forms/ContractualisationHuda.tsx
@@ -4,7 +4,6 @@ export const ContractualisationHuda = () => {
   return (
     <CustomNotice
       severity="info"
-      title=""
       className="rounded [&_p]:flex [&_p]:items-center mb-4 w-fit"
       description="Dans le cas des structures HUDA, nous vous recommandons de renouveler les conventions sur un an seulement pour anticiper la transformation prochaine de ces structures en CADA."
     />
