b0af11bf793cb60765c25c6c02fc5ee94bb73eb3
BmnQuentin <quentinbmn@gmail.com>
2026-08-11 16:05:50 +0200

    fix last note null

--- src/app/(authenticated)/(with-menu)/structures/[id]/_components/_controles/ControlesBlock.tsx
+++ src/app/(authenticated)/(with-menu)/structures/[id]/_components/_controles/ControlesBlock.tsx
@@ -32,6 +32,9 @@ export const ControlesBlock = (): ReactElement => {
   const lastPastEvaluation = getLastPastVisit(evaluations);
   const lastPastControle = getLastPastVisit(controles);
 
+  const lastNote = lastPastEvaluation?.note;
+  const hasLastNote = lastNote !== undefined && lastNote !== null;
+
   return (
     <Block
       title="Controle qualité"
@@ -48,16 +51,23 @@ export const ControlesBlock = (): ReactElement => {
         <div className="pr-4">
           <LastVisitCard evaluations={evaluations} controles={controles} />
         </div>
-        {lastPastEvaluation?.note !== undefined && (
+        {lastPastEvaluation && (
           <div className="pr-4">
             <InformationCard
               primaryInformation={
-                <>
-                  {lastPastEvaluation.note}{" "}
-                  <span className="text-xl">/&nbsp;4</span>
-                </>
+                hasLastNote ? (
+                  <>
+                    {lastNote} <span className="text-xl">/&nbsp;4</span>
+                  </>
+                ) : (
+                  "Sans note"
+                )
               }
-              secondaryInformation="de moyenne à la dernière évaluation"
+              secondaryInformation={
+                hasLastNote
+                  ? "de moyenne à la dernière évaluation"
+                  : "à la dernière évaluation"
+              }
             />
           </div>
         )}
