dddc761fc686f0c1f1e5f4bdc2b7840f00241f77
Benjamin Morali <benjaminmorali4@hotmail.fr>
2026-09-11 12:14:40 +0200

    :sparkles: Add accessibilite page (#1649)

--- src/app/components/WorkInProgress.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-
-export const WorkInProgress = (): ReactElement => {
-  return (
-    <h4 className="text-title-blue-france">👷 Construction en cours 👷</h4>
-  );
-};
