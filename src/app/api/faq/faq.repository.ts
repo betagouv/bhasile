betagouv/place-asile

:recycle: Move faq server side

src/app/api/faq/faq.repository.ts
@@ -1,0 +1,6 @@
+import prisma from "@/lib/prisma";
+
+export const getFaqItems = async () => {
+  const faqItems = await prisma.faq.findMany();
+  return { faqItems };
+};

betagouv/bhasile

src/app/api/faq/faq.repository.ts
@@ -1,0 +1,6 @@
+import prisma from "@/lib/prisma";
+
+export const findFaqItems = async () => {
+  const faqItems = await prisma.faq.findMany();
+  return { faqItems };
+};