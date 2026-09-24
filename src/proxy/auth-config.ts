8a7a5888d4f367a35b329bb4820c63fa394f22f1
BmnQuentin <quentinbmn@gmail.com>
2026-09-24 11:26:48 +0200

    fix route

--- src/proxy/auth-config.ts
+++ src/proxy/auth-config.ts
@@ -95,6 +95,12 @@ export const protectedApiRoutes: ApiRoute[] = [
     },
   },
   {
+    pattern: /^\/api\/faq$/,
+    routes: {
+      GET: "proconnect",
+    },
+  },
+  {
     pattern: /^\/api\/auth(?:\/.*)?$/,
     routes: {
       GET: "none",
